066437d848a69d30f822fdb89b9174ff9c0632e5
Eric roth <solaimanshah059@gmail.com>
2020-01-15 08:46:30 -0500

    ssh issue fixed

--- ssh.sh
+++ ssh.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Author : @you
-# web    : f
-# Script to prompt for ssh connection
-# Last Edited: July 
+# Author: @me
+# Web   : 
+# Script to prompt for ssh
 
-read -p "Which server do you want to cennect: " server_addr
-read -p "Which username do you want to use: " username
-ssh ${username}@$server_addr
+read -p "Which server do you wanna connect: " server_name
+read -p "Which username do you wanna use: " user_name
+
+echo "Loading..."
+
+# now connect with the real ssh
+ssh ${user_name}@$server_name
